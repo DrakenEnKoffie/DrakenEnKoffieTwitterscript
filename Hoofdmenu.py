85ca3447e7feac4ef9a18ba171372419381b27ee
Nicolattelater <denico438@gmail.com>
2022-01-13 13:19:28 +0100

    hoofdmeu

---
+++ Hoofdmenu.py
@@ -0,0 +1,17 @@
+print("Welcome to the twitterscript by team Draken_En_Koffie! Please select an option. "
+      "\n[1] – Download Twitter Profile "
+      "\n[2] – Generate KML from Database "
+      "\n[3] – Quit Program "
+      "\nChoose an option:")
+input()
+
+try:
+      if input() == 1:
+            #open file 1
+
+
+except ValueError:
+        print("Invalid input")
+        quit()
+
+
